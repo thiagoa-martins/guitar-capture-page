fcea7814a6152b1ea11055bc74b45e4783affdab
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-06 16:07:49 -0300

    Validate inputs

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,41 @@ button.addEventListener("click", () => {
 form.addEventListener("submit", event => {
     event.preventDefault();
     
+    let hasError = false;
     const username = event.target.username.value;
     const email = event.target.email.value;
+
+    const usernameRegex = /^[a-zA-Z ]{4,}$/;
+    const usernameIsValid = usernameRegex.test(username);
+    const emailRegex = /^[a-zA-Z0-9@.]{8,}$/;
+    const emailIsValid = emailRegex.test(email);
+    
+    if (!usernameIsValid) {
+        hasError = true;
+
+        const label = event.target.username.nextElementSibling;
+        label.textContent = `O nome de usuário deve ter somente letras e no
+        mínimo 4 caracteres.`
+    } else {
+        hasError = false;
+
+        const label = event.target.username.nextElementSibling;
+        label.textContent = "";
+    }
+
+    if (!emailIsValid) {
+        hasError = true;
+
+        const label = event.target.email.nextElementSibling;
+        label.textContent = `O e-mail deve ser no formato
+        'nomedoemail@dominio.com' e com no mínimo 8 caracteres.`;
+    } else {
+        hasError = false;
+
+        const label = event.target.email.nextElementSibling;
+        label.textContent = "";
+    }
+    
 });
 
 article.addEventListener("click", event => {
